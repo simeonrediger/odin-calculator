d60ee4edeb06721cf9999fb3fd29facb47c47e50
Simeon Rediger <simeonrediger@gmail.com>
2025-05-20 23:38:03 -0400

    Fix indentation

--- main.js
+++ main.js
@@ -19,12 +19,12 @@ document.querySelector('#buttons').addEventListener('click', handleButtonClick);
 function displayCurrentTime() {
     const timeDisplay = document.querySelector('#time');
     timeDisplay.textContent = new Date()
-    .toLocaleString([], {
-        hour: '2-digit',
-        minute: '2-digit',
-    })
-    .replace(/\s+(AM|PM)/, '')    // Remove meridiem indicator
-    .replace(/0(?=[0-9]:)/, '');  // Remove left-padding 0 from hour
+        .toLocaleString([], {
+            hour: '2-digit',
+            minute: '2-digit',
+        })
+        .replace(/\s+(AM|PM)/, '')    // Remove meridiem indicator
+        .replace(/0(?=[0-9]:)/, '');  // Remove left-padding 0 from hour
 }
 
 function handleButtonClick(event) {
